04581727cdff15d7acf9ad2bacc6b2bc5705826f
Norbertas <nlapenas@gmail.com>
2023-03-24 18:13:37 +0000

    implemented log4j2 for basic logging

--- src/main/kotlin/RedTrackerMain.kt
+++ src/main/kotlin/RedTrackerMain.kt
@@ -1,30 +1,38 @@
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.requests.GatewayIntent
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.core.config.Configurator
 import tasks.text.TextTaskManager
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 
 //TODO Add logger
 /**
  * @args[0] is the discord API TOKEN.
  * @args[1] is the Database link.
  * @args[2] is the Database Username and Password separated by a space.
- * @args[3] OPTIONAL(Default = 0) logging level
- *  0 - Only Errors
  */
 fun main(args: Array<String>){
-    if(args.size >= 4){
-        //TODO set logging level
-    }else if(args.size < 3){
-        //TODO Error Msg
+    val logger: Logger = LoggerFactory.getLogger("RedTrackerMain.main")
+    Configurator.setRootLevel(Level.ALL)
+
+    if(args.size < 3){
+        logger.error("Missing args in the main function!\n" +
+                "@args[0] is the discord API TOKEN.\n" +
+                "@args[1] is the Database link.\n" +
+                "@args[2] is the Database Username and Password separated by a space.")
         //return
     }
     //TODO Set Database
     //TODO Set Database Credentials
 
-    val jda: JDA = JDABuilder.createLight(args[0],
+    JDABuilder.createLight(args[0],
         GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
         .addEventListeners(TextTaskManager())
         .build().awaitReady()
 
-    println("\u001B[32mRed Tracker is ready to go!")
+    logger.info("Red Tracker is ready to go!")
+
 }
